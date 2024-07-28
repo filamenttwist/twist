2ee0eee452315843823bbf712fa69dc3da65efeb
Karim Mohamed <komicho1996@gmail.com>
2024-07-28 18:12:15 +0300

    Feat: create a service provider

---
+++ src/TwistServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Obelaw\Twist;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwistServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
