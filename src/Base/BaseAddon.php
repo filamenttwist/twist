ef1f292d48df657ccfb3a5e531fa79f68b77f154
Karim Mohamed <komicho1996@gmail.com>
2024-07-31 00:03:15 +0300

    Feat: create a base for the addon

---
+++ src/Base/BaseAddon.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Obelaw\Twist\Base;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+
+abstract class BaseAddon implements Plugin
+{
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public function getId(): string
+    {
+        return class_basename($this);
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+}
