9d481d81c3f31ad2d03edfb98bf1d257d33cb29f
Karim Mohamed <komicho1996@gmail.com>
2024-08-26 09:37:42 +0300

    Feat: implement migrate rollback

--- src/Console/MigrateCommand.php
+++ src/Console/MigrateCommand.php
@@ -9,16 +9,29 @@ use Obelaw\Twist\Facades\Twist;
 
 final class MigrateCommand extends Command
 {
-    protected $signature = 'twist:migrate';
+    protected $signature = 'twist:migrate {--r|rollback : rollback}';
 
     protected $description = 'Modules setup';
 
     public function handle(): void
     {
+        $migratePaths = [];
         foreach (Twist::getModules() as $module) {
             if (method_exists($module, 'pathMigrations')) {
-                dump($module->pathMigrations());
+                array_push($migratePaths, $module->pathMigrations());
             }
         }
+
+        if ($this->option('rollback')) {
+            $this->call('migrate:rollback', [
+                '--path' => $migratePaths,
+            ]);
+
+            return;
+        }
+
+        $this->call('migrate', [
+            '--path' => $migratePaths,
+        ]);
     }
 }
