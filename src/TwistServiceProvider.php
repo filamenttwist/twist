c02ec8031b10eeaa450b6fb9e8cd332311d80deb
Karim Mohamed <komicho1996@gmail.com>
2025-01-04 11:22:32 +0200

    Feat: add `SetupAddonCommand` for addon setup functionality

--- src/TwistServiceProvider.php
+++ src/TwistServiceProvider.php
@@ -5,6 +5,7 @@ namespace Obelaw\Twist;
 use Illuminate\Support\ServiceProvider;
 use Obelaw\Twist\Classes\TwistClass;
 use Obelaw\Twist\Console\MigrateCommand;
+use Obelaw\Twist\Console\SetupAddonCommand;
 use Obelaw\Twist\Console\SetupCommand;
 
 class TwistServiceProvider extends ServiceProvider
@@ -34,6 +35,7 @@ class TwistServiceProvider extends ServiceProvider
 
         $this->commands([
             SetupCommand::class,
+            SetupAddonCommand::class,
             MigrateCommand::class,
         ]);
     }
