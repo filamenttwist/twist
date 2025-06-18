17dad5ac262b18bb5ed86f59b3281840d17eec63
Karim Mohamed <komicho1996@gmail.com>
2025-06-18 14:34:09 +0300

    Feat: implement `Strand` class for managing a collection of providers

---
+++ src/Strand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Obelaw\Twist;
+
+/**
+ * Strand
+ *
+ * This class is used to manage a collection of providers.
+ * It allows setting, adding, and retrieving providers based on conditions.
+ */
+class Strand
+{
+    private static $providers = [];
+
+    public static function setProviders(array $provider)
+    {
+        static::$providers = $provider;
+
+        return new static;
+    }
+
+    public function addProvider(callable $condition, string|array $provider)
+    {
+        if ($condition()) {
+            static::$providers = array_merge(static::$providers, (array)$provider);
+        }
+
+        return $this;
+    }
+
+    public function getProviders()
+    {
+        return static::$providers;
+    }
+}
